e03a6b6ff791cef39795c8be3ef34513cd8c5b27
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-08-25 14:16:22 -0500

    get global call to display correctly to the console

--- global.js
+++ global.js
@@ -1,1 +1,7 @@
-console.log(`Versions: ${process.versions.Node} ${process.versions.V8}`);
+const version = {
+  node: 'Node.js version: v6.3.1',
+  V8: 'V8 version: 5.0.71.57'
+};
+
+console.log(`${version.node}
+${version.V8}`);
